7753bb0e19b2b1ddee3cd432dd77d1e91787c202
billkabanga <tkbillkabanga@gmail.com>
2019-10-07 16:03:57 +0300

    setup empty react project

---
+++ README.md
@@ -0,0 +1,1 @@
+#### UI revamp for the Landreg application.
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const title = 'Taka Lyo App'
+
+ReactDOM.render(
+  <App title={title}/>,
+  document.getElementById('root')
+);
+
+module.hot.accept();
