00bc248573d47f422e5a8932c21ebbc523475b2e
billkabanga <tkbillkabanga@gmail.com>
2019-10-08 15:00:34 +0300

    chore(redux): setup redux
    - add redux dependencies to the project
    - create redux file structure
    - create store for the project
    
    [delivers #168982724]

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from '../redux/store';
 import Home from '../components/home';
 import Login from '../components/login';
 
+const store = configureStore();
+
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/home" component={Home} exact/>
-        <Route path="/login" component={Login}/>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home" component={Home} exact/>
+          <Route path="/login" component={Login}/>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
